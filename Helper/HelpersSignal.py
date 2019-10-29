a14a005f5eb5b4d11fb2eb68303b21b616c62f4c
Daniel Hufschläger <daniel@hufschlger.net>
2019-10-29 13:24:19 +0100

    Adding shifted phase feature for phase controlled waveforms.

--- Helper/HelpersSignal.py
+++ Helper/HelpersSignal.py
@@ -10,13 +10,13 @@ def pulse_train(t, at, shape):
     """create a train of pulses over $t at times $at and shape $shape"""
     return np.sum(shape(t - at[:,np.newaxis]), axis=0)
 
-def phaseshifting ( alpha, f, t ):
+def phaseshifting ( alpha, f, t , phase = 0):
     """ Creates a phase controlled waveform. alpha describes the switching point """
     omega = np.pi * 2 * f
     w = alpha / omega
     d = np.arange ( .5/f, t[-1], .5/f )
-    d = d - .5*w;
-    y = np.sin( omega * t);
+    d = d - .5*w - phase/omega;
+    y = np.sin( omega * t + phase);
 
     y2 = pulse_train(
         t=t,                           # time domain
