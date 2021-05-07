e1f3de7826bcc391ee968f2d6ea9af1c24f3da17
Daniel Hufschläger <daniel@hufschlaeger.net>
2021-05-07 15:25:43 +0200

    Adding pressure calculation

--- thermo_acoustic_coating_thickness.py
+++ thermo_acoustic_coating_thickness.py
@@ -32,6 +32,7 @@ sub_therm_cp = 78.75
 gas_heat_cond = 0.026
 gas_density =  1.24
 gas_therm_cp = 1004
+c_gas = 343
 
 ## film constants
 film_density = 7120
@@ -41,7 +42,18 @@ film_therm_cp = 233
 e_gas = np.sqrt(gas_heat_cond * gas_density * gas_therm_cp )
 e_sub = np.sqrt(sub_heat_cond * sub_density * sub_therm_cp )
 
-f = np.linspace ( 0, 1e6, 10000 )
+f = np.linspace ( 10, 1e6, 10000 )
+
+v_th = 2 *np.pi * np.power ( 343 / f , 3.0 ) / 3
+
+R = 10
+I = 1e-5
+fprf = 20
+pw = 100e-9
+
+P = I**2 / R
+
+Qin = P * ( 1 / fprf ) / ( pw  )  
 
 layer_thickness = np.array ( (100e-9, 200e-9, 300e-9 ))
 set_style()
@@ -50,11 +62,13 @@ fig, ax = plt.subplots(1)
 
 for iThickness in layer_thickness:
     r = e_gas / ( e_gas + e_sub + iThickness * film_density * film_therm_cp * np.sqrt( 2* np.pi* f) )
-    ax.plot ( f*1e-3, 20*np.log10(r), label=str(int(iThickness*1e9)) + ' nm' )
+    p = r * Qin / (v_th ) 
+    ax.plot ( f*1e-3, 20*np.log10(p/20e-6), label=str(int(iThickness*1e9)) + ' nm' )
 
 ax.legend()
 ax.set_xlabel( r'Frequency / kHZ' )
-ax.set_ylabel( r'Amplitude / dB' )
+ax.set_ylabel( r'Sound pressure level (SPL) / dB' )
+ax.set_ylim ( 0, 160 )
 ax.grid(True)
 
 set_size(fig)
