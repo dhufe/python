bc35697e1e3a44b991abcf00b757931e027ac3c3
Daniel Hufschläger <daniel@hufschlaeger.net>
2021-05-07 06:23:30 +0200

    Eye candy.

--- numpy_mmap/load_data.py
+++ numpy_mmap/load_data.py
@@ -5,8 +5,8 @@ import zipfile
 NX = 0
 NY = 0
 # colormap
-colormap = 'inferno'
-fileName = 'C:\\Users\\dhufschl\\OneDrive - Bundesanstalt für Materialforschung und -prüfung (BAM)\\FTDT-PALLUP\\2D_FTDT_Result_6.npz'
+colormap = 'coolwarm'
+fileName = 'C:\\Users\\dhufschl\\OneDrive - Bundesanstalt für Materialforschung und -prüfung (BAM)\\FTDT-PALLUP\\2D_FTDT_Result_8.npz'
 
 def npz_headers(npz):
     """Takes a path to an .npz file, which is a Zip archive of .npy files.
@@ -52,8 +52,8 @@ SourceGap = nFile['SourceGap']
 NAperture = dx * ( NSources * ( SourceGap + SourceWidth ) - SourceGap )
 RNearField = np.power(NAperture , 2.0 ) * f / ( 4 * c )
 
-print ( 'Recorded propagation times: %f us' % ( NFrames * dt * 1e6 ) )
-print ( 'Recorded propagation distance : %f mm' % ( NFrames * dt * c * 1e3 ) )
+print ( 'Recorded propagation times: %f us' % ( NFrames * 50 * dt * 1e6 ) )
+print ( 'Recorded propagation distance : %f mm' % ( NFrames * 50 * dt * c * 1e3 ) )
 print ( 'Aperture : %f mm ' %( NAperture * 1e3) )
 print ( 'Points needed : %d' %( RNearField / dx ) )
 print ( 'Nearfield distance : %f mm' % (RNearField*1e3 ) )
@@ -65,11 +65,16 @@ NXEnd   = NX#//2 + int( .5 * RNearField / dx )
 NYStart = 0#100 + 10
 NYEnd   = NY#100 + int( .5 * RNearField / dx ) + 10
 
-x = np.arange ( 0, (NX)*dx, dx )
+x = np.arange ( -(NX//2)*dx, (NX//2)*dx, dx )
 y = np.arange ( 0, (NY)*dx, dx )
 xx, yy = np.meshgrid( x, y )
 
 fig, ax = plt.subplots(1)
-ax.pcolor ( xx[NYStart:NYEnd,NXStart:NXEnd], yy[NYStart:NYEnd,NXStart:NXEnd], np.mean(nFile['P'][:,NYStart:NYEnd,NXStart:NXEnd], axis=0) , vmin=-1, vmax=1, cmap=colormap, shading='auto')
-plt.savefig('FTDT_Test_6.png', dpi=300)
+Psum = np.sum ( nFile['P'][0:300,NYStart:NYEnd,NXStart:NXEnd] , axis = 0)
+Pmax = np.amax( Psum )
+Pmin = np.amin( Psum )
+print ( Psum / Pmax )
+#print ( Psum / Pmin )
+ax.pcolor ( xx[NYStart:NYEnd,NXStart:NXEnd], yy[NYStart:NYEnd,NXStart:NXEnd], Psum , vmin=-1, vmax=1, cmap=colormap, shading='auto')
+plt.savefig('FTDT_Test_8.png', dpi=300)
 plt.show()
